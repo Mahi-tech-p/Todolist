d7b5071f1a43eb5ac63c9a489cc70ed68099d4a5
Mahi-tech-p <leocodes01@gmail.com>
2024-09-16 15:57:29 +0530

    changes in code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import './App.css'
+import TodoInput from './Components/TodoInput'
+import TodoList from './Components/TodoList'
 function App() {
+  const [listTodo, setlistTodo] = useState([])
+
+  let addList = (inputText) => {
+    if( inputText !== '')
+    setlistTodo([...listTodo, inputText])
+  }
+  const deleteListitem = (key) => {
+    let newListTodo = [...listTodo]
+    newListTodo.splice(key, 1)
+    setlistTodo([...newListTodo])
+  }
   return (
-    <div className="App">
-    
+    <div className='main-container'>
+      <div className='center-container'>
+        <TodoInput addList={addList} />
+        <h1 className='app-heading'>TODO</h1>
+        <hr />
+        {listTodo.map((listItem, i) => {
+          return (
+            <TodoList key={i} index={i} item={listItem} deleteItem ={deleteListitem} />
+          )
+
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
